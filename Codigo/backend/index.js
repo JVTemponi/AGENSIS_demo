e3ca8f184ba3e0263df63556314af1dbec7910f9
João Victor Temponi <jvictortemponi@gmail.com>
2024-12-09 22:30:47 -0300

    Update index.js

--- Codigo/backend/index.js
+++ Codigo/backend/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Para JSON
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 //Importação das rotas
 const PacotesRoutes = require('./routes/PacotesRoutes');
